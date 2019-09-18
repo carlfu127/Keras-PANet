605f05d6efb86abfe4df697c4b88a620cf173162
CLF <liangfu127@163.com>
2019-09-18 16:37:33 +0800

    Update config.py

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -1,12 +1,3 @@
-"""
-Mask R-CNN
-Base Configurations class.
-
-Copyright (c) 2017 Matterport, Inc.
-Licensed under the MIT License (see LICENSE for details)
-Written by Waleed Abdulla
-"""
-
 import numpy as np
 
 
